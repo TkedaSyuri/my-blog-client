5cf64f27c926c919161277d1700abb8d27b8ddc7
Takeda Syuri <30nanchikusyuri@gmail.com>
2024-09-05 13:09:59 +0900

    レイアウトを変更

--- src/app/(main)/record/page.tsx
+++ src/app/(main)/record/page.tsx
@@ -14,21 +14,35 @@ const page = async () => {
   const posts = await getPosts();
   return (
     <main>
-      <h2 className="text-2xl font-bold">記録一覧</h2>
+      <h2 className="text-2xl font-bold">Blog一覧</h2>
+    <button className="bg-purple-600">
+      <Link href="record/edit">
+      投稿する
+      </Link> 
+      </button>
       <div className="mt-7">
-      <div className="grid gap-8">
-        {posts.map((post) => (
-          <div key={post.id} className="bg-gradient-to-r   from-red-600  to-gray-800  p-5 mx-10">
-            <Link
-              href={`record/post/${post.id}`}
-              className="text-2xl hover:text-blue-600 cursor-default"
+        <div className="grid gap-8">
+          {posts.map((post) => (
+            <div
+              key={post.id}
+              className="bg-gradient-to-r   from-red-700  to-gray-700  p-5 mx-10"
             >
-              <div className="text-white">{post.title}</div>
-            </Link>
-            <div className="font-semibold ">作成日 : {post.created_at}</div>
-          </div>
-        ))}
-      </div>
+              <div className="text-2xl hover:text-blue-600 cursor-default">
+                <div className="text-white">{post.title}</div>
+              </div>
+              <div className="font-semibold ">作成日 : {post.created_at}</div>
+              <div className="">
+                <Link href={`record/post/${post.id}`}>
+                  <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-gray-600 group-hover:from-purple-600 group-hover:to-red-700 hover:text-white dark:text-white focus:ring-4 focus:outline-none  ">
+                    <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                    blogを見る
+                    </span>
+                  </button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
